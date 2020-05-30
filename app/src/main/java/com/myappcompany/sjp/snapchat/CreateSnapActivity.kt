4dd3958ea084d3eb88c497873f7699ac21c17163
jpsagarm95 <jpsagarm95@gmail.com>
2020-05-29 22:59:15 -0700

    Uploading image using firebase

--- app/src/main/java/com/myappcompany/sjp/snapchat/CreateSnapActivity.kt
+++ app/src/main/java/com/myappcompany/sjp/snapchat/CreateSnapActivity.kt
@@ -1,12 +1,98 @@
 package com.myappcompany.sjp.snapchat
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import com.google.firebase.storage.UploadTask
+import com.google.android.gms.tasks.OnSuccessListener
+import android.support.annotation.NonNull
+import com.google.android.gms.tasks.OnFailureListener
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.storage.FirebaseStorage
+import java.io.ByteArrayOutputStream
+import java.util.*
+
 
 class CreateSnapActivity : AppCompatActivity() {
+
+    var createSnapsImageView: ImageView? = null
+    var messageEditText: EditText? = null
+    val imageName = UUID.randomUUID().toString() + ".jpg"
+
+    fun getPhoto() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, 1)
+    }
+
+    fun nextClicked(view: View) {
+        createSnapsImageView?.setDrawingCacheEnabled(true)
+        createSnapsImageView?.buildDrawingCache()
+        val bitmap = (createSnapsImageView?.getDrawable() as BitmapDrawable).bitmap
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+
+
+
+        val uploadTask = FirebaseStorage.getInstance().getReference().child("images").child(imageName).putBytes(data)
+        uploadTask.addOnFailureListener(OnFailureListener {
+            // Handle unsuccessful uploads
+            Toast.makeText(this, "Upload Failed!!!", Toast.LENGTH_SHORT).show()
+        }).addOnSuccessListener(OnSuccessListener<UploadTask.TaskSnapshot> { taskSnapshot ->
+            // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+            val downloadUrl = taskSnapshot.downloadUrl
+            Log.i("Info", downloadUrl.toString())
+        })
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == 1) {
+            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getPhoto()
+            }
+        }
+    }
+
+    fun chooseImageClicked(view: View) {
+        if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+        } else {
+            getPhoto()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        val selectedImage = data!!.data
+
+        if(requestCode == 1 && resultCode == Activity.RESULT_OK && data != null) {
+            try {
+                val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, selectedImage)
+                createSnapsImageView?.setImageBitmap(bitmap)
+            } catch (e: Exception) {
+
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_snap)
+
+        createSnapsImageView = findViewById(R.id.createSnapImageView)
+        messageEditText = findViewById(R.id.messageEditText)
     }
 }
